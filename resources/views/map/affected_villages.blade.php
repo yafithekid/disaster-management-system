ee3309783d5b72785df50d487ca64f88388e408a
Hayyu hanifah <heripottypot@gmail.com>
2016-05-09 18:05:11 +0700

    change form position

---
+++ resources/views/map/affected_villages.blade.php
@@ -0,0 +1,24 @@
+@extends('map.master')
+@section('form')
+    <script>var refresh2 = function(){
+            var villageId = 10346;
+            $.get( "/dimas/medical-facilities?villageId="+villageId, function( data ) {
+                console.log(data);
+            });
+        }
+    </script>
+    <label>Village ID</label>
+    <input id="village_id"/>
+    <a onclick="refresh2()" class="btn btn-primary">Filter</a>
+@endsection
+@section('script')
+<script>
+    var refresh = function(){
+        var villageId = 10346;
+        $.get( "/dimas/medical-facilities?villageId="+villageId, function( data ) {
+            console.log(data);
+        });
+    }
+
+</script>
+@endsection
