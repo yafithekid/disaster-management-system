ab07559d1f42d1d63b019fa23002c63b9b699560
stanleysantoso <stanleysantoso@students.itb.ac.id>
2016-05-11 14:55:29 +0700

    add query & result set for victim movements

--- resources/views/map/victim_movements.blade.php
+++ resources/views/map/victim_movements.blade.php
@@ -6,34 +6,77 @@
 @endsection
 @section('script')
 <script>
-    var victimData;
-    var layerGroup = L.layerGroup();
+    
+    var victimData; 
+    var sql;
+    var layerGroup = new L.layerGroup();
     var refresh = function(){
-       
-        var victimId = $('#village_id').val();  
-        // var TIMEOUT=1000;
-        var coordinates = [];
-
+        $("#resultSet").empty();
+        var victimId = $('#village_id').val();
         $.get( "/dimas/victim-movements/"+victimId, function( data ) {
-            layerGroup.clearLayers();
-            victimData = data;
+            var coordinates = [];
+            var x,y;            
+            victimData = data.resultSet;
+            sql = data.executedQuery;
+            
+            if(layerGroup.getLayers().length>0){
+                layerGroup.clearLayers();
+            }
+
+            $("#resultSet").append(addTypeDiv(victimData[0]));
             victimData.forEach(function(datum){
-                console.log(datum.point);
-                var geoJson = L.geoJson(datum.point);
-                var x = datum.point.coordinates[1];
-                var y = datum.point.coordinates[0];
+                var marker = new L.geoJson(datum.point);
+                x = datum.point.coordinates[1];
+                y = datum.point.coordinates[0];
                 coordinates.push([x,y]);
-                // setTimeout(function(){
-                mymap.panTo(new L.LatLng(x,y));
-                mymap.setZoom(13);
-                layerGroup.addLayer(geoJson).addTo(mymap);
-                // },TIMEOUT);   
+                layerGroup.addLayer(marker);
+                var datumDiv = addDatumDiv(datum);
+                $("#resultSet").append(datumDiv);
             }); 
             
-            var pathline = L.polyline(coordinates)
-            layerGroup.addLayer(pathline).addTo(mymap);
-        
+            mymap.setZoom(4);
+            mymap.panTo(new L.LatLng(x,y));
+            mymap.setZoom(13);
+            var pathline = new L.polyline(coordinates);
+            layerGroup.addLayer(pathline);
+            layerGroup.addTo(mymap);
+            $("#executedQuery").html(sql);
         });
     }
+
+    function addTypeDiv( data ){
+        var div = $(document.createElement('div'));
+        div.attr("class","row");
+        div.css("background-color",'#CCC');
+        div.css("border","1px solid #AAA");
+        for (var key in data){
+            var tempDiv = $(document.createElement('div'));
+            tempDiv.attr("id",key);
+            tempDiv.attr("class","col-md-1");
+            tempDiv.append(key);
+            div.append(tempDiv);
+        }
+        return div;
+    }
+    function addDatumDiv( data ){
+        var div = $(document.createElement('div'));
+        div.attr("class","row");
+        div.css("background-color",'#DDD');
+        div.css("border","1px solid #AAA");
+        for (var key in data){
+            var tempDiv = $(document.createElement('div'));
+            tempDiv.attr("id",key);
+            tempDiv.attr("class","col-md-1");
+            if(key=='point'){
+                tempDiv.append(data[key].coordinates);
+            }
+            else{
+                tempDiv.append(data[key]);
+            }
+            div.append(tempDiv);    
+        }
+        return div;
+    }
+    $("#resultSet").css("padding-bottom", "10px");
 </script>
 @endsection
